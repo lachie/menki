982a721c49d527350a53c92385db70150567c088
Tim Lucas <t.lucas@toolmantim.com>
2008-04-13 20:34:24 +1000

    Cleaned up some init comments

--- config/init.rb
+++ config/init.rb
@@ -5,7 +5,6 @@ Gem.path.unshift(Merb.root / "gems")
 # Make the app's "lib" directory a place where ruby files get "require"d from
 $LOAD_PATH.unshift(Merb.root / "lib")
 
-
 Merb::Config.use do |c|
   
   ### Sets up a custom session id key, if you want to piggyback sessions of other applications
@@ -16,32 +15,8 @@ Merb::Config.use do |c|
   c[:session_store] = 'cookie'
 end  
 
-### Merb doesn't come with database support by default.  You need
-### an ORM plugin.  Install one, and uncomment one of the following lines,
-### if you need a database.
-
-### Uncomment for DataMapper ORM
 use_orm :datamapper
-
-### Uncomment for ActiveRecord ORM
-# use_orm :activerecord
-
-### Uncomment for Sequel ORM
-# use_orm :sequel
-
-
-### This defines which test framework the generators will use
-### rspec is turned on by default
-###
-### Note that you need to install the merb_rspec if you want to ue
-### rspec and merb_test_unit if you want to use test_unit. 
-### merb_rspec is installed by default if you did gem install
-### merb.
-###
-# use_test :test_unit
 use_test :rspec
-
-### Add your other dependencies here
 
 # These are some examples of how you might specify dependencies.
 # 
@@ -51,10 +26,8 @@ use_test :rspec
 # OR
 # dependencies "RedCloth" => "> 3.0", "ruby-aes-cext" => "= 1.0"
 
-# dependency "haml" - broken?
-require 'merb-haml' 
+require 'merb-haml' # dependency "haml" - broken?
 
 Merb::BootLoader.after_app_loads do
-  ### Add dependencies here that must load after the application loads:
   require 'menki_config'
 end
