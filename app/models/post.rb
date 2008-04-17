a0f7765c17edb4750c3e50406da153db5feedbe3
Tim Lucas <t.lucas@toolmantim.com>
2008-04-18 00:45:31 +1000

    Add Post body formatting errors validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,25 @@ class Post < DataMapper::Base
   property :published_at, :datetime, :index => true
   property :updated_at, :datetime
   property :format, :string, :nullable => false
+
+  validates_true_for :body, :logic => (Proc.new do
+    begin
+      formatted_body
+      true
+    rescue Exception => e
+      errors.add(:body, "has a formatting error: #{e.message}")
+      false
+    end
+  end)
+
+  def to_param
+    self.slug
+  end
   
   def formatted_body
     Formatter.format(self.format, self.body)
   end
-  def to_param
-    self.slug
-  end
+
   def updated_or_published_at
     updated_at || published_at
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,8 @@ describe Post do
   it "formatted_body should return the formatted body" do
     Post.new(:body => "*Hello*", :format => "markdown").formatted_body.should eql("<p><em>Hello</em></p>")
   end
+  it "should validate well formatted body" do
+    lambda { Formatter.format(:haml, "- boom!") }.should raise_error
+    Post.new(:format => "haml", :body => "- boom!").should_not be_valid(:body)
+  end
 end
