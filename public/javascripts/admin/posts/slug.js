5116ce7ca9a88bbd65b341b3ad85c7266a6dd31b
Tim Lucas <t.lucas@toolmantim.com>
2008-04-18 23:06:14 +1000

    Fix auto-slug function so it correctly returns false

--- public/javascripts/admin/posts/slug.js
+++ public/javascripts/admin/posts/slug.js
@@ -1,9 +1,11 @@
+// Adds an (auto) link next to the slug field to auto-fill it based on the
+// post's title
 $(function(){
-  $('<a href="#">(auto)</a>').click(function(event) {
-    event.stopPropagation();
+  $('<a href="#">(auto)</a>').insertAfter("input#post_slug").click(function(event) {
     $("input#post_slug").attr(
       "value",
-      $("input#post_title").attr("value").toLowerCase().split(/\ +/).join("-").replace(/\-+/, "-").replace(RegExp("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-]"), "")
+      $("input#post_title").attr("value").toLowerCase().split(/\ +/).join("-").replace(RegExp("[^abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-]"), "").replace(/\-+/, "-")
     );
-  }).insertAfter("input#post_slug");
+    return false;
+  });
 });
